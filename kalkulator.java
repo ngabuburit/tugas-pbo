9e50ecbc3cae56b9240890c5a48093c1b00c13b3
tony <80567881+ngabuburit@users.noreply.github.com>
2021-03-13 19:14:37 +0700

    first commit

---
+++ kalkulator.java
@@ -0,0 +1,15 @@
+public class kalkulator{
+    public static void main(String[]args){
+        int a = 10, b = 5;
+        int tambah = a+b;
+        int kurang = a-b;
+        int bagi = a/b;
+        int kali = a*b;
+        int modulo = a%b;
+        System.out.println("hasil jumlah : " +tambah);
+        System.out.println("hasil kurang : " +kurang);
+        System.out.println("hasil bagi : " +bagi);
+        System.out.println("hasil kali : " +kali);
+        System.out.println("mod : " +modulo);
+    }
+}
